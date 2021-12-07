399cff97d8771271f492787cd40bb2e9a6250d94
Caleb Van Lue <cpvanlue@bsu.edu>
2021-12-07 12:22:33 -0500

    Fix audio stop after selecting Quit in pause menu

--- PauseMenu/PauseMenu.gd
+++ PauseMenu/PauseMenu.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
 
+onready var mainMenuTheme := SoundPlayer.get_node("MainMenuTheme")
+onready var levelMusic := SoundPlayer.get_node("LevelMusic")
+
 func _on_Resume_pressed() -> void:
 	$AnimationPlayer.play_backwards("pause"); yield($AnimationPlayer, "animation_finished")
 	$Resume.disabled = true
@@ -9,6 +12,9 @@ func _on_Resume_pressed() -> void:
 
 func _on_Quit_pressed() -> void:
 	get_parent().get_tree().paused = false
+	var volume = levelMusic.volume_db
+	SoundPlayer.fade(levelMusic.get_node("Tween"), levelMusic, volume, -80, Tween.EASE_IN, true, false)
+	SoundPlayer.fade(mainMenuTheme.get_node("Tween"), mainMenuTheme, -80, volume, Tween.EASE_OUT, false, true)
 	SceneTransition.change_scene("MainMenu/MainMenu.tscn")
 
 
